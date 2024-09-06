241283eafe3ef8a81cdad04e39cee9258b534752
shirinze <sherize1373@gmail.com>
2024-09-06 15:53:44 +0300

    create buiness layer

---
+++ SignalR.BuinessLayer/Concreate/SocialMediaManager.cs
@@ -0,0 +1,45 @@
+﻿using SignalR.BuinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BuinessLayer.Concreate
+{
+    public class SocialMediaManager : ISocialMediaService
+    {
+        private readonly ISocialMediaDal _socialmediaDal;
+        public SocialMediaManager(ISocialMediaDal socialmediaDal)
+        {
+            _socialmediaDal = socialmediaDal;
+        }
+        public void TAdd(SocialMedia entity)
+        {
+            _socialmediaDal.Add(entity);
+
+        }
+
+        public void TDelete(SocialMedia entity)
+        {
+            _socialmediaDal.Delete(entity);
+        }
+
+        public SocialMedia TGetByID(int id)
+        {
+            return _socialmediaDal.GetByID(id);
+        }
+
+        public List<SocialMedia> TGetListAll()
+        {
+            return _socialmediaDal.GetListAll();
+        }
+
+        public void TUpdate(SocialMedia entity)
+        {
+            _socialmediaDal.Update(entity);
+        }
+    }
+}
