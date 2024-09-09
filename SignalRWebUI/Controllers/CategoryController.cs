9228d1d4a48f13db3a974542f3f7eba6e60bbc44
shirinze <sherize1373@gmail.com>
2024-09-09 14:23:08 +0300

    added category list to web ui

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
 
 namespace SignalRWebUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpclientfactory;
+        public CategoryController(IHttpClientFactory httpclientfactory)
         {
+            _httpclientfactory = httpclientfactory; 
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpclientfactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7006/api/Category");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
