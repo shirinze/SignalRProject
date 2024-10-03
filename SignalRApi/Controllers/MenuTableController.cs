cb2a9a436164e53651c8bbecbf71166d6df58894
shirinze <sherize1373@gmail.com>
2024-10-03 14:39:38 +0300

    menutable işlemleri

--- SignalRApi/Controllers/MenuTableController.cs
+++ SignalRApi/Controllers/MenuTableController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BuinessLayer.Abstract;
+using SignalR.DtoLayer.MenuTableDto;
+using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
 {
@@ -19,6 +21,53 @@ namespace SignalRApi.Controllers
 		public IActionResult MenuTableCount()
 		{
 			return Ok(_menutableService.TMenuTableCount());
+		}
+
+		[HttpGet]
+		public IActionResult MenuTableList()
+		{
+			var values = _menutableService.TGetListAll();
+			return Ok(values);
+		}
+
+		[HttpPost]
+		public IActionResult CreateMenuTable(CreateMenuTableDto createmenutabledto)
+		{
+			MenuTable menutable = new MenuTable()
+			{
+				Status = false,
+				Name = createmenutabledto.Name,
+			
+			};
+			_menutableService.TAdd(menutable);
+			return Ok("basarili bir sekilde eklendi");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteMenuTable(int id)
+		{
+			var value = _menutableService.TGetByID(id);
+			_menutableService.TDelete(value);
+			return Ok("basari bir sekilde silindi");
+		}
+		[HttpPut]
+		public IActionResult UpdateMenuTable(UpdateMenuTableDto updatemenutabledto)
+		{
+			MenuTable menuTable = new MenuTable
+			{
+				MenuTableID = updatemenutabledto.MenuTableID,
+				Name = updatemenutabledto.Name,
+				Status = false,
+			
+			};
+			_menutableService.TUpdate(menuTable);
+			return Ok("guncellemdi");
+		}
+		[HttpGet("{id}")]
+		public IActionResult GetAbout(int id)
+		{
+			var value = _menutableService.TGetByID(id);
+			return Ok(value);
 		}
 	}
 }
---
+++ SignalRWebUI/Controllers/MenuTableController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.MenuTableDtos;
+using System.Text;
+
+namespace SignalRWebUI.Controllers
+{
+	public class MenuTableController : Controller
+	{
+		private readonly IHttpClientFactory _httpclientFactory;
+
+		public MenuTableController(IHttpClientFactory httpclientFactory)
+		{
+			_httpclientFactory = httpclientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpclientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7006/api/MenuTable");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var value = JsonConvert.DeserializeObject<List<ResultMenuTableDto>>(jsonData);
+				return View(value);
+			}
+			return View();
+		}
+		[HttpGet]
+		public async Task<IActionResult> CreateMenuTable()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> CreateMenuTable(CreateMenuTableDto createmenutabledto)
+		{
+			var client = _httpclientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createmenutabledto);
+			StringContent stringcontent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7006/api/MenuTable", stringcontent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public async Task<IActionResult> DeleteMenuTable(int id)
+		{
+			var client = _httpclientFactory.CreateClient();
+			var responseMessage = await client.DeleteAsync($"https://localhost:7006/api/MenuTable/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+		[HttpGet]
+		public async Task<IActionResult> UpdateMenuTable(int id)
+		{
+			var client = _httpclientFactory.CreateClient();
+			var responseMessage = await client.GetAsync($"https://localhost:7006/api/MenuTable/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var value = JsonConvert.DeserializeObject<UpdateMenuTableDto>(jsonData);
+				return View(value);
+			}
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> UpdateMenuTable(UpdateMenuTableDto updatemenutabledto)
+		{
+			var client = _httpclientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(updatemenutabledto);
+			StringContent stringcontent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PutAsync("https://localhost:7006/api/MenuTable", stringcontent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+	}
+	
+}
