b098ad4a3003a96a6a9bfb7e21271bb512d64579
shirinze <sherize1373@gmail.com>
2024-10-10 13:54:31 +0300

    Authentication işlemleri tamamlandı

--- SignalRWebUI/Program.cs
+++ SignalRWebUI/Program.cs
@@ -1,14 +1,25 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using SignalR.DataAccessLayer.Concreate;
 using SignalR.EntityLayer.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var requireAutorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
 // Add services to the container.
 builder.Services.AddDbContext<SignalRContext>();
 builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<SignalRContext>();
 builder.Services.AddHttpClient();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAutorizePolicy));
+});
 
+builder.Services.ConfigureApplicationCookie(opts =>
+{
+    opts.LoginPath = "/Login/Index/";
+}); 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,7 +34,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
---
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutSocialMediaPartialComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.SocialMediaDdtos;
+
+namespace SignalRWebUI.ViewComponents.UILayoutComponents
+{
+	public class _UILayoutSocialMediaPartialComponent:ViewComponent
+	{
+		private readonly IHttpClientFactory _httpclientfactory;
+
+		public _UILayoutSocialMediaPartialComponent(IHttpClientFactory httpclientfactory)
+		{
+			_httpclientfactory = httpclientfactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpclientfactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7006/api/SocialMedia");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData=await responseMessage.Content.ReadAsStringAsync();
+				var value = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+				return View(value);
+			}
+			return View();
+		}
+	}
+}
